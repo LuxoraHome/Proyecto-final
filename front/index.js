d9dbdd594c5b731e58ef8955868c8c199643bf9c
LuxoraHome <luxoradecorationhome@gmail.com>
2025-02-21 20:58:43 -0300

    final

--- front/index.js
+++ front/index.js
@@ -1,3 +1,13 @@
-function laCajaDePandora(numero){
+function laCajaDePandora(numero) {
     // proximamente escribiremos codigo aqui
+}
+
+const TomasKMorelli = () => {
+    const datos = {
+        name: "Tomas Morelli",
+        edad: "24",
+        nacionalidad: "Argentino"
     }
+    return datos;
+
+}
