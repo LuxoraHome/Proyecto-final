7ed5c63afca49c68004ce6ffc07a2a7cd2217f65
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-02-21 19:16:52 -0500

    Cambio a rama danielbackend

--- front/index.js
+++ front/index.js
@@ -1,7 +1,22 @@
-function laCajaDePandora(numero){
-    if (numero % 2 === 0 ) {
+function laCajaDePandora(numero) {
+    if (numero % 2 === 0) {
         return numero.toString(2)
     } else {
         return numero.toString(16)
     }
+}
+function laCajaDePandora(numero) {
+    // proximamente escribiremos codigo aqui
+}
+
+const DanielVelazco = () => {
+
+    const datos = {
+        name: 'Daniel Velazco',
+        edad: "35",
+        nacionalidad: "Colombiano"
     }
+
+}
+
+return datos;
