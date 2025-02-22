a56cde1155519e18e40539866975f6e4eec7efef
Cladinoc18 <citylavivas@gmail.com>
2025-02-21 19:06:41 -0500

    miRama

--- front/index.js
+++ front/index.js
@@ -1,3 +1,11 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function convertirNumero(numero) {
+    if (numero % 2 === 0) {
+        return numero.toString(2); 
+    } else {
+        return numero.toString(16); 
     }
+}
+
+
+console.log(convertirNumero(10)); 
+console.log(convertirNumero(15)); 
