67bd0ea70b3ae4131cff665938b662871b1ab91e
Raul Vanegas <raulvanegas711@gmail.com>
2025-02-21 19:07:15 -0500

    funcionPrueba

--- front/index.js
+++ front/index.js
@@ -1,3 +1,11 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+    if (!Number.isInteger(numero)) {
+        return "Por favor, ingresa un número entero.";
     }
+    
+    if (numero % 2 === 0) {
+        return numero.toString(2); // Convierte a binario
+    } else {
+        return numero.toString(16); // Convierte a hexadecimal
+    }
+}
