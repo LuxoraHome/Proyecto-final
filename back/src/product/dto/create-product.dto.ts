c05acab729d2f83defd924add928229ccd547527
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-02-24 21:54:40 -0500

    Se crea dto product

--- back/src/product/dto/create-product.dto.ts
+++ back/src/product/dto/create-product.dto.ts
@@ -1,1 +1,12 @@
-export class CreateProductDto {}
+export class CreateProductDto {
+
+    id: string
+    name: string
+    description: string
+    type: string
+    price: number
+    designer: string
+    image: string
+    stock: number
+
+}
