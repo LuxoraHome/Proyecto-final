516d5ec3efd6db9406862a38c45d83a156f25f7e
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-02-24 22:00:52 -0500

    Se crea entities product

--- back/src/product/entities/product.entity.ts
+++ back/src/product/entities/product.entity.ts
@@ -1,1 +1,17 @@
-export class Product {}
+import { Generated } from "typeorm"
+import { v4 as uuid } from 'uuid'
+
+export class Product {
+
+    @Generated('uuid')
+    id: string = uuid()
+
+    name: string
+    description: string
+    type: string
+    price: number
+    designer: string
+    image: string
+    stock: number
+
+}
