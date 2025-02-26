3b5c035a20f368ee412cdf1c787f29ddc82e9fe8
LuxoraHome <luxoradecorationhome@gmail.com>
2025-02-26 19:55:21 -0300

    cart1

--- front/src/components/cartCompenent/CartComponent.tsx
+++ front/src/components/cartCompenent/CartComponent.tsx
@@ -9,14 +9,14 @@ export const CartComponent: React.FC = () => {
     const [cart, setCart] = useState<iProducts[]>([])
     const [price, setPrice] = useState<number>(0)
 
-    
+
     useEffect(() => {
-        const products : iProducts[] = JSON.parse(localStorage.getItem("cart") || "[]")
-       
-        
-        
+        const products: iProducts[] = JSON.parse(localStorage.getItem("cart") || "[]")
+
+
+
         if (products) {
-       const total = products.reduce((acc , items:iProducts)=> acc + items.price  ,0)
+            const total = products.reduce((acc, items: iProducts) => acc + items.price, 0)
             setCart(products)
             setPrice(total)
         }
@@ -25,28 +25,33 @@ export const CartComponent: React.FC = () => {
 
 
     return (
+
         <div>
-
+            <h2 className="semi-bold mt-4 text-4xl tracking-wide ml-6">Cart</h2>
             {cart ? (cart.map((items) => (
-                <div key={items.id}>
-                    <h3>{items.name}</h3>
-                    <img src={items.image} />
+                
+                <div key={items.id} className=" flex flex-row  items-center content-center gap-4 p-6 border border-gray-300 rounded-lg shadow-sm mt-6 w-1/2 ml-6" >
+                    <img src={items.image} className="w-64 h-36 object-cover rounded-md" />
+                    <div className="flex flex-col">
+                    <h3 className="font-bold">{items.name}</h3>
                     <h3>{items.product}</h3>
-                    <h3>${items.price}</h3>
-
+                    <h3 className="font-bold">${items.price}</h3>
+                    </div>
                 </div>
             ))) : (<div>
                 <h3>You dont have products</h3>
             </div>)}
 
             <div>
-                <h3> total account: ${price}</h3>
+            <h3>  Account: ${price}</h3>
             </div>
 
+            
         </div>
+
     )
 
 
 }
 
-export default CartComponent ;
+export default CartComponent;
