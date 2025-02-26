38dfd0851e80991d61bc3061d1d76f7e7f9ed816
Raul Vanegas <raulvanegas711@gmail.com>
2025-02-26 00:17:15 -0500

    userSeeder

--- back/src/config/typeOrm.ts
+++ back/src/config/typeOrm.ts
@@ -13,34 +13,15 @@ const config = {
   port: parseInt(process.env.DB_PORT, 10),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-
   autoLoadEntities: true,
   synchronize: true,
-  logging: true,
+  // dropSchema: true,
+  // logging: true,
   entities: ['dist/**/*.entity{.ts,.js}'],
-  migrations: ['dist/migration/*{.ts,.js}'],
-
+  migrations: ['dist/migration/*{.ts,.js}']
 }
 
 
 export default registerAs("typeorm", () => config)
 
 export const connectionsSource = new DataSource(config as DataSourceOptions)
-
-
-//   port: process.env.DB_PORT,
-//   username: process.env.DB_USERNAME,
-//   password: process.env.DB_PASSWORD,
-//   autoLoadEntities: true,
-//   synchronize: true,
-//   // dropSchema: true,
-//   logging: true,
-//   // entities: ['dist/**/*.entity{.ts,.js}'],
-//   // migrations: ['dist/migrations/*{.js,.ts}'],
-// }
-
-
-// export default registerAs("typeorm", () => config)
-// export const connectionsSource = new DataSource (config as DataSourceOptions)
-
-
--- back/src/main.ts
+++ back/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { UserSeed } from './user/seeder/user.seed';
 
 
 async function bootstrap() {
@@ -14,6 +15,10 @@ async function bootstrap() {
     .build();
   const documet = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, documet);
+
+  const userSeeder = app.get(UserSeed);
+  await userSeeder.createUserSeeder();
+  console.log('La inserción de usuarios ha terminado');
 
   await app.listen(process.env.PORT ?? 3000);
 }
