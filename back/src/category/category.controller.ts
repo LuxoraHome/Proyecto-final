cc86c6169ab4d3389ab5ce8a2f22f7b87d0ffe52
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-02-26 16:24:25 -0500

    Se modifica el create-category.dto

--- back/src/category/category.controller.ts
+++ back/src/category/category.controller.ts
@@ -2,10 +2,12 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Category')
 @Controller('category')
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
+  constructor(private readonly categoryService: CategoryService) { }
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
