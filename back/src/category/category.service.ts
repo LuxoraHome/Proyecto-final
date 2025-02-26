7a3e54a365b165df141724feb8a17558523ea9d7
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-02-26 16:44:45 -0500

    Se crea la ruta POST /category

--- back/src/category/category.service.ts
+++ back/src/category/category.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+
+  constructor(
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>
+  ) { }
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    const newCategoty = this.categoryRepository.create(createCategoryDto);
+    return this.categoryRepository.save(newCategoty);
   }
 
   findAll() {
