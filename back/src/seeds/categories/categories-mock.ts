1c63ccdac38f87ea2fddeeea8c76c0fa1b4a623e
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-02-26 22:23:16 -0500

    Se crea Seed Categories

---
+++ back/src/seeds/categories/categories-mock.ts
@@ -0,0 +1,10 @@
+export const categories: string[] = [
+
+    'Living Room',
+    'Dining Room',
+    'Desk',
+    'Bedroom',
+    'Outdoor Furniture',
+    'Other products'
+
+]
--- back/src/seeds/seeds.module.ts
+++ back/src/seeds/seeds.module.ts
@@ -1,19 +1,22 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { Category } from "src/category/entities/category.entity";
 import { Product } from "src/product/entities/product.entity";
+import { CategoriesSeed } from "./categories/categories.seeds";
 import { ProductsSeed } from "./products/products.seeds";
 
 @Module({
     imports: [TypeOrmModule.forFeature([
+        Category,
         Product
     ])],
-
     providers: [
-        ProductsSeed,
+        CategoriesSeed,
+        ProductsSeed
     ],
-
     exports: [
-        ProductsSeed,
+        CategoriesSeed,
+        ProductsSeed
     ]
 })
 
