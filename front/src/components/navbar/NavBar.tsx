8b5f8eff8ed626b28cb972e45ee930ae25b9d0e9
LuxoraHome <luxoradecorationhome@gmail.com>
2025-02-27 21:56:29 -0300

    navBar

--- front/src/components/navbar/NavBar.tsx
+++ front/src/components/navbar/NavBar.tsx
@@ -2,6 +2,11 @@
 
 import React from "react";
 import { PiShoppingBag } from "react-icons/pi";
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoLogOutOutline } from "react-icons/io5";
+import { FaRegUser } from "react-icons/fa"; import { RiLogoutCircleRLine } from "react-icons/ri";
+
+
 import Link from "next/link";
 
 
@@ -11,20 +16,37 @@ export const Navbar: React.FC = async () => {
 
   return (
 
-    <nav>
+    <nav className="bg-white flex items-center justify-between px-6 py-4 border-b border-black">
 
-      <div className="text-gray-500 order-3 w-full md:w-auto md:order-2 items-center">
-        <ul className="flex justify-center items-center font-semibold text-center">
-          <Link href="/">
-            <h1>LUXORA</h1>
-          </Link>
-        </ul>
-      </div>
-      <div className=" flex-1 md:order-3 flex justify-end items-center order-3">
-        <Link href="/cart">
-          <h3>Cart</h3>
-          <PiShoppingBag className="order-2 " />
+      <div className="flex flex-col items-start">
+        <Link href="/">
+          <h1 className="text-4xl font-semibold font-mono tracking-wide">LUXORA</h1>
         </Link>
+        <h3 className="text-gray-600 text-xl self-end mt-1">Paris</h3>
+      </div>
+
+      <div className="flex items-center justify-center flex-grow mx-4">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-80 p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+        />
+      </div>
+
+      <div className="flex items-center space-x-6 text-2xl text-gray-800">
+        <Link href="/">
+          <FaRegUser />
+        </Link>
+        <Link href="/cart">
+          <PiShoppingBag />
+        </Link>
+        <Link href="/">
+          <FaRegUserCircle />
+        </Link>
+        <button >
+          <IoLogOutOutline  className="text-2xl font-bold  text-gray-800 "/>
+        </button>
+
       </div>
 
     </nav>
