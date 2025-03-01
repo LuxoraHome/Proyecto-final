c70633d6d644790488ff985b13a6eb67c6edd2d3
LuxoraHome <luxoradecorationhome@gmail.com>
2025-03-01 02:44:12 -0300

    t

--- front/src/helpers/users.ts
+++ front/src/helpers/users.ts
@@ -1,6 +1,6 @@
 import { IUserLogin, IUserRegister } from "@/interfaces/Iuser";
 
-const APIURL = process.env.NEXT_PUBLIC_API_URL
+const APIURL= process.env.NEXT_PUBLIC_API_URL
 
 
 export const RegisterUser = async (userData: IUserRegister) => {
@@ -17,7 +17,7 @@ export const RegisterUser = async (userData: IUserRegister) => {
         }
 
     } catch (error) {
-        alert("login fail")
+     
         console.log(`aqui esta el error ${error}`);
 
     }
