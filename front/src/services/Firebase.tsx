3d20212a1f41dd51da237d8a9f7867b980a440dc
LuxoraHome <luxoradecorationhome@gmail.com>
2025-03-05 15:13:29 -0300

    m

--- front/src/services/Firebase.tsx
+++ front/src/services/Firebase.tsx
@@ -1,16 +1,34 @@
 import { initializeApp } from "firebase/app";
-import { GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB8V2cFqEV8vc9N9fYxYi3zLuPS8RqbHVA",
-  authDomain: "luxora-proyectofinal.firebaseapp.com",
-  projectId: "luxora-proyectofinal",
-  storageBucket: "luxora-proyectofinal.firebasestorage.app",
-  messagingSenderId: "378605874445",
-  appId: "1:378605874445:web:bd8ec31a5f79b40d409444",
-  measurementId: "G-ZV345BC8W8"
+    apiKey: "AIzaSyB8V2cFqEV8vc9N9fYxYi3zLuPS8RqbHVA",
+    authDomain: "luxora-proyectofinal.firebaseapp.com",
+    projectId: "luxora-proyectofinal",
+    storageBucket: "luxora-proyectofinal.firebasestorage.app",
+    messagingSenderId: "378605874445",
+    appId: "1:378605874445:web:bd8ec31a5f79b40d409444",
+    measurementId: "G-ZV345BC8W8"
 };
 
 
 initializeApp(firebaseConfig);
+const auth = getAuth()
+
+
+const googleProvider = new GoogleAuthProvider()  // 1 defino mi proveedor/es externo
+
+export const authProvider = async (provider: GoogleAuthProvider) => {  // 2 creo funcion async donde solicito la interaccion de mi proveedor.
+
+    try {
+        const response = await signInWithPopup(auth, provider)   //signInWithPopup crea el modulo mail de google
+        console.log(response);
+        
+
+    } catch (error) {
+        console.log(`aca esta el error`, error);
+
+    }
+
+}
