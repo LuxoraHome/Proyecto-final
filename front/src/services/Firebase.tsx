a0074fc361083972d64cf89586368ae8c3784167
LuxoraHome <luxoradecorationhome@gmail.com>
2025-03-05 15:04:25 -0300

    c

---
+++ front/src/services/Firebase.tsx
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8V2cFqEV8vc9N9fYxYi3zLuPS8RqbHVA",
+  authDomain: "luxora-proyectofinal.firebaseapp.com",
+  projectId: "luxora-proyectofinal",
+  storageBucket: "luxora-proyectofinal.firebasestorage.app",
+  messagingSenderId: "378605874445",
+  appId: "1:378605874445:web:bd8ec31a5f79b40d409444",
+  measurementId: "G-ZV345BC8W8"
+};
+
+
+initializeApp(firebaseConfig);
