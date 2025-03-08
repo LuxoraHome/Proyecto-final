e5ec6a156685177722235460ad264b65209f35a4
gianluqqa <gianlucacaravone55@gmail.com>
2025-03-08 16:41:50 -0300

    c

---
+++ front/src/app/orders/page.tsx
@@ -0,0 +1,12 @@
+import OrdersDashboardView from '@/views/OrdersDashboardView'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <OrdersDashboardView/>
+    </div>
+  )
+}
+
+export default page
