fa062baf318ad3c59ae59f8dc2228538c3edd682
LuxoraHome <luxoradecorationhome@gmail.com>
2025-03-08 00:03:04 -0300

    c

--- front/src/components/profile/page.tsx
+++ front/src/components/profile/page.tsx
@@ -4,7 +4,8 @@ import React from 'react';
 const page = () => {
   return (
     <div>
-      <h1>Aca va el profile view</h1>
+      <h1>Aca va el profile vieeew</h1>
+      <h2>gian</h2>
       <ProfileView />
     </div>
   );
