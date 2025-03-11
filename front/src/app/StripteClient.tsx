a34e28f94437086d316648fc5baa867ceac8eea9
LuxoraHome <luxoradecorationhome@gmail.com>
2025-03-11 18:47:52 -0300

    c

--- front/src/app/StripteClient.tsx
+++ front/src/app/StripteClient.tsx
@@ -4,11 +4,21 @@
 
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import React from "react";
 
 const API_PUBLIC = process.env.NEXT_PUBLIC_PUBLIC_KEY;
 
 const stripePromise = loadStripe(`${API_PUBLIC}`);
 
-export default function StripeClient({ children }: { children: React.ReactNode }) {
-  return <Elements stripe={stripePromise}>{children}</Elements>;
+
+interface IStripe {
+  children : React.ReactNode
 }
+
+export  const StripeClient : React.FC<IStripe> = ({children})=>{
+return (<Elements stripe={stripePromise}>{children}</Elements>)
+
+}
+
+
+export default StripeClient ;
