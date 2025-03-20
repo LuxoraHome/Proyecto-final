cde18a7c02d85504ebdcf046a99056550e3e67d7
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-03-20 18:44:29 -0500

    Se elimina restricciones en el registro de usuario

--- back/src/auth/dto/create-auth.dto.ts
+++ back/src/auth/dto/create-auth.dto.ts
@@ -15,127 +15,72 @@ import { UserClient } from 'src/user/enum/userClient.enum';
 import { Role } from '../enum/roles.enum';
 import { UserStatus } from 'src/user/enum/userStatus.enum';
 
-
 export class CreateAuthDto {
-  @ApiProperty({
-    description: 'Nombre del usuario',
-    minLength: 3,
-    maxLength: 80,
-    example: 'Juan Pérez',
-  })
+  @ApiProperty({ description: 'Nombre del usuario', minLength: 3, maxLength: 80, example: 'Juan Pérez', required: false })
   @IsString()
-  @IsNotEmpty()
-  @Length(3, 80)
-  name: string;
+  @IsOptional()
+  //@Length(3, 80)
+  name?: string = "";
 
-  @ApiProperty({
-    description: 'UID único del usuario',
-    example: '123e4567-e89b-12d3-a456-426614174000',
-  })
+  @ApiProperty({ description: 'UID único del usuario', example: '123e4567-e89b-12d3-a456-426614174000', required: false })
   @IsString()
-  @IsNotEmpty()
+  //@IsOptional()
   uid: string;
 
-  @ApiProperty({
-    description: 'Correo electrónico del usuario',
-    example: 'usuario@example.com',
-  })
+  @ApiProperty({ description: 'Correo electrónico del usuario', example: 'usuario@example.com', required: false })
   @IsEmail()
+  //@IsOptional()
   email: string;
 
-  @ApiProperty({
-    description:
-      'Contraseña con al menos una letra mayúscula, una minúscula, un número y un carácter especial',
-    minLength: 8,
-    maxLength: 15,
-    example: 'Password123!',
-  })
+  @ApiProperty({ description: 'Contraseña', minLength: 8, maxLength: 15, example: 'Password123!', required: false })
   @IsString()
-  @Matches(
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,15}$/,
-    {
-      message:
-        'password must contain at least one lowercase letter, one uppercase letter, one number, and one special character',
-    },
-  )
-  @Length(8, 15)
-  password: string;
+  //@Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,15}$/, {
+  //  message: 'password must contain at least one lowercase letter, one uppercase letter, one number, and one special character',
+  //})
+  @IsOptional()
+  //@Length(8, 15)
+  password?: string = "Mundial@123";
 
-  @ApiProperty({
-    description: 'Confirmación de la contraseña',
-    minLength: 8,
-    maxLength: 15,
-    example: 'Password123!',
-  })
-  @IsNotEmpty()
-  @Length(8, 15)
-  confirmPassword: string;
+  @ApiProperty({ description: 'Confirmación de la contraseña', minLength: 8, maxLength: 15, example: 'Password123!', required: false })
+  @IsOptional()
+  //@Length(8, 15)
+  confirmPassword?: string = "Mundial@123";
 
-  @ApiProperty({
-    description: 'Dirección del usuario',
-    minLength: 3,
-    maxLength: 80,
-    example: 'Calle 123, Ciudad, País',
-  })
+  @ApiProperty({ description: 'Dirección del usuario', minLength: 3, maxLength: 80, example: 'Calle 123, Ciudad, País', required: false })
   @IsString()
-  @Length(3, 80)
-  address: string;
+  @IsOptional()
+  //@Length(3, 80)
+  address?: string = "";
 
-  @ApiProperty({
-    description: 'Número de teléfono del usuario',
-    example: '+1234567890',
-  })
-  @IsNotEmpty()
+  @ApiProperty({ description: 'Número de teléfono del usuario', example: '+1234567890', required: false })
+  @IsOptional()
   @IsString()
-  phone: string;
+  phone?: string = "";
 
-  @ApiProperty({
-    description: 'País de residencia',
-    minLength: 5,
-    maxLength: 20,
-    example: 'México',
-  })
+  @ApiProperty({ description: 'País de residencia', minLength: 5, maxLength: 20, example: 'México', required: false })
   @IsString()
-  @Length(5, 20)
-  country: string;
+  @IsOptional()
+  //@Length(5, 20)
+  country?: string = "";
 
-  @ApiProperty({
-    description: 'Ciudad de residencia',
-    minLength: 5,
-    maxLength: 20,
-    example: 'Ciudad de México',
-  })
+  @ApiProperty({ description: 'Ciudad de residencia', minLength: 5, maxLength: 20, example: 'Ciudad de México', required: false })
   @IsString()
-  @Length(5, 20)
-  city: string;
+  @IsOptional()
+  //@Length(5, 20)
+  city?: string = "";
 
-
-  
-  @ApiProperty({
-    description: 'Tipo de cliente (STANDARD, PREMIUM)',
-    enum: UserClient,
-    example: UserClient.STANDARD,
-  })
+  @ApiProperty({ description: 'Tipo de cliente', enum: UserClient, example: UserClient.STANDARD, required: false })
   @IsEnum(UserClient)
   @IsOptional()
-  client?: UserClient;
-  
-  @ApiProperty({
-    description: 'Tipo de usuario (USER, ADMIN, SUPERADMIN)',
-    enum: Role,
-    example: Role.User,
-  })
+  client?: UserClient = UserClient.STANDARD;
+
+  @ApiProperty({ description: 'Tipo de usuario', enum: Role, example: Role.User, required: false })
   @IsEnum(Role)
   @IsOptional()
-  role?: Role;
+  role?: Role = Role.User;
 
-  @ApiProperty({
-    description: 'Tipo de status (ACTIVE, SUSPENDED)',
-    enum: UserStatus,
-    example: UserStatus.ACTIVE,
-  })
+  @ApiProperty({ description: 'Tipo de status', enum: UserStatus, example: UserStatus.ACTIVE, required: false })
   @IsEnum(UserStatus)
   @IsOptional()
-  status?: UserStatus;
-
+  status?: UserStatus = UserStatus.ACTIVE;
 }
