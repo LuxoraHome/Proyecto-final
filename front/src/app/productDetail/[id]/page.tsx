a3d4d687c7a355c179679ef8d36664c9dec6249b
TomasKMorelli <totimorelli123@gmail.com>
2025-03-25 20:24:19 -0300

    c

--- front/src/app/productDetail/[id]/page.tsx
+++ front/src/app/productDetail/[id]/page.tsx
@@ -1,35 +1,20 @@
-import { useEffect, useState } from "react";
 import { getProductsId } from "@/helpers/getProducts";
 import ProductsDetailId from "@/components/productDetail/ProductDetailId";
-import { iProducts } from "@/interfaces/iProducts";
 
 interface PageProps {
   params: { id: string };
 }
 
-const Page: React.FC<PageProps> = ({ params }) => {
-  const [productData, setProductData] = useState<iProducts | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
+export default async function Page({ params }: PageProps) {
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getProductsId(params.id);
-      setProductData(data);
-      setLoading(false);
-    };
+  
+  const data = await getProductsId(params.id);
 
-    fetchData();
-  }, [params.id]);
+  console.log("este id recibo en params", params.id);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (!productData) {
+  if (!data) {
     return <div>Product not found</div>;
   }
 
-  return <ProductsDetailId productData={productData} />;
-};
-
-export default Page;
+  return <ProductsDetailId productData={data} />;
+}
