f38d07525a7f50e25ceeb2e4ca549de3ca55c77a
TomasKMorelli <totimorelli123@gmail.com>
2025-03-25 13:33:16 -0300

    c

--- front/src/components/productDetail/ProductDetailId.tsx
+++ front/src/components/productDetail/ProductDetailId.tsx
@@ -1,31 +1,40 @@
-
 import { iParams } from "@/interfaces/iParams";
 import { getProductsId } from "@/helpers/getProducts";
 import React from "react";
 import RenderProductDetail from "./RenderProductDetail";
 
 
+export const ProductsDetailId: React.FC<{ name: string; image: string; price: number; product: string; description: string; id: string }> = ({ name, image, price, product, description, id }) => {
+  return (
+    <div>
+      <RenderProductDetail
+        name={name}
+        image={image}
+        price={price}
+        product={product}
+        description={description}
+        id={id}
+      />
+    </div>
+  );
+};
 
 
-export const ProductsDetailId: React.FC<iParams> = async ({ params }) => {
+export const getServerSideProps = async ({ params }: { params: iParams['params'] }) => {
+  const data = await getProductsId(params.id); 
 
+  const { name, image, price, product, description, id } = data;
 
-    const data = await getProductsId(params.id)
-
-   
-
-    const { name, image, price, product, description, id } = data
-
-    return (
-
-        <div>
-            <RenderProductDetail name={name} image={image} price={price} product={product} description={description} id={id} />
-        </div>
-
-    )
-
-
-
-}
+  return {
+    props: {
+      name,
+      image,
+      price,
+      product,
+      description,
+      id,
+    },
+  };
+};
 
 export default ProductsDetailId;
