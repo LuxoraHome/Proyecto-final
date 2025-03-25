bb83fccdd844753b9edbaa8f188c9a37656aa71d
TomasKMorelli <totimorelli123@gmail.com>
2025-03-25 15:16:42 -0300

    c

--- front/src/components/productDetail/ProductDetailId.tsx
+++ front/src/components/productDetail/ProductDetailId.tsx
@@ -1,31 +1,52 @@
 
+"use client"
+
 import { iParams } from "@/interfaces/iParams";
 import { getProductsId } from "@/helpers/getProducts";
 import React from "react";
 import RenderProductDetail from "./RenderProductDetail";
-
+import { useEffect, useState } from "react";
+import { iProducts } from "@/interfaces/iProducts";
 
 
 
 export const ProductsDetailId: React.FC<iParams> = async ({ params }) => {
 
 
-    const data = await getProductsId(params.id)
-    if (!data) {
-        return (
-            <div>Product not found</div>
-        )
-    }
+  const [data, setData] = useState<iProducts | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
 
-    const { name, image, price, product, description, id } = data
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const productData = await getProductsId(params.id);
+        if (productData) {
+          setData(productData);
+        } else {
+          setError("Product not found");
+        }
+      } catch (error) {
+        setError("An error occurred while fetching product details");
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    return (
+    fetchData();
+  }, [params.id]);
+  if (!data) {
+    return <div>Product not found</div>
+  }
+  const { name, image, price, product, description, id } = data
 
-        <div>
-            <RenderProductDetail name={name} image={image} price={price} product={product} description={description} id={id} />
-        </div>
+  return (
 
-    )
+    <div>
+      <RenderProductDetail name={name} image={image} price={price} product={product} description={description} id={id} />
+    </div>
+
+  )
 
 
 
