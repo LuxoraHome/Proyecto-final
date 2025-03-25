f43b1a008aaff8ea90b7dcb6dc68ef4c9d9fc9fb
TomasKMorelli <totimorelli123@gmail.com>
2025-03-25 19:16:41 -0300

    c

--- front/src/app/productDetail/[id]/page.tsx
+++ front/src/app/productDetail/[id]/page.tsx
@@ -5,6 +5,9 @@ export default async function Page({ params }: { params: { id: string } }) {
  
   const data = await getProductsId(params.id);
 
+  console.log('este id recibo en params', params.id);
+  
+
   if (!data) {
     return <div>Product not found</div>;
   }
