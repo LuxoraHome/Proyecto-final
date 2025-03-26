2b4ab6338c9072c145008ada20f50b1ea82c847d
TomasKMorelli <totimorelli123@gmail.com>
2025-03-26 00:55:57 -0300

    c

--- front/src/interfaces/Iuser.ts
+++ front/src/interfaces/Iuser.ts
@@ -2,8 +2,8 @@
 
 export interface IUserRegister {
     name?: string,
-    email?: string,
-    password?: string,
+    email: string,
+    password: string,
     confirmPassword?: string,
     address?: string,
     phone?: string,
