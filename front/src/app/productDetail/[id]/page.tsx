c984f9f078889a8114f82c43002903bdb207b9a5
TomasKMorelli <totimorelli123@gmail.com>
2025-03-25 21:08:54 -0300

    c

--- front/src/app/productDetail/[id]/page.tsx
+++ front/src/app/productDetail/[id]/page.tsx
@@ -1,14 +1,15 @@
 import { getProductsId } from "@/helpers/getProducts";
 import ProductsDetailId from "@/components/productDetail/ProductDetailId";
 
-
-
 export default async function Page({ params }: { params: { id: string } }) {
 
-  const data = await getProductsId(params.id);
+  const { id } = params;
+
+  
+  const data = await getProductsId(id);
 
   console.log("Producto recuperado:", data);
-  console.log("ID de params recibido:", params.id);
+  console.log("ID de params recibido:", id);
 
   if (!data) {
     return <div>Product not found</div>;
