f13a64a41e24066e73e5c84d40c1d856a5caa039
TomasKMorelli <totimorelli123@gmail.com>
2025-03-26 01:27:53 -0300

    c

--- front/src/views/ProfileView.tsx
+++ front/src/views/ProfileView.tsx
@@ -29,9 +29,7 @@ const ProfileView: React.FC = () => {
           <p className="text-gray-700 mb-3 text-center">
             <span className="font-bold">Name:</span> {user.displayName || user.name}
           </p>
-          <p className="text-gray-700 mb-3 text-center">
-            <span className="font-bold">Email:</span> {user.email}
-          </p>
+       
 
           {user.address && (<p className="text-gray-700 mb-3 text-center">
             <span className="font-bold">Address:</span> {user.address}
